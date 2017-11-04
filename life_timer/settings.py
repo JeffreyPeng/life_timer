935502aef060b3d47da789454690b8ab081559d4
JeffreyPeng <pyx718@gmail.com>
2017-11-04 23:34:09 +0800

    tmp

--- life_timer/settings.py
+++ life_timer/settings.py
@@ -123,7 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'collected_static'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
